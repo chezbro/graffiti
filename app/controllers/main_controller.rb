230dec07d3503272a535ffc2546560fce3c6de73
William Crouse <williamcrouse@Williams-MacBook.local>
2014-01-13 14:42:24 -0600

    Made graffiti background image

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,8 @@
 class MainController < UIViewController
   def viewDidLoad
     super
-    image = (568 == UIScreen.mainScreen.bounds.size.height) ? ("640_1136.png") : ("640_1136.png")
-    image_view = UIImageView.alloc.initWithImage(UIImage.imageNamed(image))
-
-    self.view.addSubview(image_view)
-
-
+    @image_view = UIImageView.alloc.initWithImage(UIImage.imageNamed("640_1136.png"))
+    @image_view.frame = [[0, 0], [UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height]]
+    self.view.addSubview(@image_view)
   end
 end
